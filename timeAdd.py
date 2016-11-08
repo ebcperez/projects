dcecfb068cbe18a67b79425ea2e2a8c468cd989f
Earl Perez <ebcperezcsc@gmail.com>
2016-11-08 13:48:37 -0800

    Update timeAdd.py

--- timeAdd.py
+++ timeAdd.py
@@ -1,11 +1,16 @@
-cont = True
-total = 0
-print('Enter times')
-while cont:
-    time = input()
-    if time == 'end':
-        cont = False
-    mins = int(time[0:2])*60
-    secs = int(time[2:4])
-    total += mins + secs
-print(total//60, 'and', total%60,'seconds')
+
+def timeAdd():
+    total = 0
+    print('Enter times(type "end" to end operation):')
+    while True:
+        time = input()
+        if time == 'end':
+            break
+        mins = int(time[0:2])*60
+        secs = int(time[2:4])
+        total += mins + secs
+    totalMin = total//60
+    totalSecs = total%60
+    return  str(totalMin) + ' mins and ' + str(totalSecs) + ' seconds '
+
+print(timeAdd())
